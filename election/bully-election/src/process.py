09e76160c73c0138a48c2ad13cc88db6efb862ca
Harunari Takata <tkt0430@keio.jp>
2025-09-04 22:02:19 +0900

    done skeleton

--- election/bully-election/src/process.py
+++ election/bully-election/src/process.py
@@ -8,15 +8,7 @@ class Process(multiprocessing.Process):
         self.is_leader = False
         self.leader_id = None
         self.process_list = []
-    
-    
+
 
     def run(self):
-        print(f"[Process {self.id}] 起動しました。")
-        try:
-            while True:
-                time.sleep(2)
-        except KeyboardInterrupt:
-            pass 
-        finally:
-            print(f"[Process {self.id}] 終了します。")
+        pass
